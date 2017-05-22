36bd25fbb1666b7d61645d20bd207679d423827a
golodnyi <ochen@golodnyi.ru>
2017-05-22 20:19:05 +0700

    Display params of test

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { TestsComponent } from './tests/tests.component';
-import { MdListModule } from '@angular/material';
+import { MdCardModule, MdListModule, MdToolbarModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TestsService } from '../../services/tests.service';
 import { TestComponent } from './test/test.component';
@@ -12,7 +12,9 @@ import { TestComponent } from './test/test.component';
     CommonModule,
     DashboardRoutingModule,
     MdListModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MdToolbarModule,
+    MdCardModule
   ],
   declarations: [TestsComponent, TestComponent],
   providers: [TestsService]
